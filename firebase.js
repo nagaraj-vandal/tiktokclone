76c05145b6cdf8daf4fd46e880d8dbb77958db0b
freelancers <freelancersjji@gmail.com>
2020-08-02 14:56:29 +0530

    first commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCWBKfmjCKx3fxhhqd4Mkbd5YcTC0rbNLw",
+    authDomain: "tiktokclone-5e915.firebaseapp.com",
+    databaseURL: "https://tiktokclone-5e915.firebaseio.com",
+    projectId: "tiktokclone-5e915",
+    storageBucket: "tiktokclone-5e915.appspot.com",
+    messagingSenderId: "471893573678",
+    appId: "1:471893573678:web:846eea3938719cf51adfbf",
+    measurementId: "G-ZJL9CEHBPB"
+};
